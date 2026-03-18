8aa09ab99d7c5cc3b979a7b8fd14142313646946
pratik <pratikdmane2006@gmail.com>
2026-03-18 12:09:26 +0530

    Add files via upload

---
+++ basiccoding.py
@@ -0,0 +1,31 @@
+# import re
+# var='gasgg54@3vscd!s*'
+# count=0
+# for i in var:
+#     z = re.findall('[a-z,0-9]',i)
+#     z=ord(i)
+#     print(z)
+#     if z:
+#         if z>97 and z<122:
+#             continue
+#         elif z>=48 and z<=57:
+#             continue
+#         else:
+#             count=count+1
+# print(count)
+
+
+# list=[0, 1, 0, 3, 12]
+# for i in list :
+#     if i==0:
+#         list.remove(i)
+#         list.append(i)
+# print(list)
+
+
+list=[7,3,9,2,8]
+list.sort()
+print(list)
+print(list[-2])
+            
+            
